a1a281a93ae23cf3f92a47921feb9d59dbee8407
Hajk <tunczorx@gmail.com>
2018-10-05 10:34:30 +0200

    Add project files.

---
+++ LearningASP.NET_MVC_V0.0.1/Global.asax.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace LearningASP.NET_MVC_V0._0._1
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+        }
+    }
+}
